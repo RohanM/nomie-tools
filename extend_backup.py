7be067d7cd3baafd6bc58a62e3d002324beed62c
Rohan Mitchell <rohan@rohanmitchell.com>
2017-08-21 21:02:05 +1000

    Put trackers in groups / boards

--- extend_backup.py
+++ extend_backup.py
@@ -3,6 +3,9 @@
 #
 # Usage:
 # python extend_backup.py -i <backup.json> -o <backup.new.json> -t <trackers.csv>
+#
+# Trackers CSV format:
+# board,tracker
 
 import sys, getopt
 import csv
@@ -28,6 +31,8 @@ backup = NomieBackup(backup_filename)
 with open(trackers_filename) as f:
     csvreader = csv.reader(f)
     for row in csvreader:
-        backup.add_tracker(row[0])
+        # Row contains: group,tracker
+        tracker_id = backup.add_tracker(row[1])
+        backup.add_tracker_to_group(tracker_id, row[0])
 
 backup.save(output_filename)
--- nomie_backup.py
+++ nomie_backup.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 class NomieBackup:
     def __init__(self, filename):
@@ -6,8 +7,25 @@ class NomieBackup:
             self.data = json.loads(f.read())
 
     def add_tracker(self, name):
-        self.data['trackers'].append({'label': name})
+        id = self._random_id()
+        self.data['trackers'].append({'_id': id, 'label': name})
+        return id
+
+    def add_tracker_to_group(self, tracker_id, group_name):
+        groups = self.data['meta'][self._groups_meta_index()]['groups']
+        if group_name not in groups:
+            groups[group_name] = []
+        groups[group_name].append(tracker_id)
 
     def save(self, filename):
         with open(filename, 'w') as f:
             f.write(json.dumps(self.data))
+
+    def _random_id(self):
+        # 13 digits decimal - 6 digits hex
+        # eg. 0123456789012-abcdef
+        return "{:013d}-{:06x}".format(random.randint(0, 9999999999999), random.randint(0, 16777215))
+
+    def _groups_meta_index(self):
+        # Find meta section with id "hyperStorage-groups"
+        return [i for i, x in enumerate(self.data['meta']) if x['_id'] == "hyperStorage-groups"][0]
